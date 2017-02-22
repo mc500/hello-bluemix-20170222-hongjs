3d1653ae3712ef56792a34852d6bb92c91691af5
HONGJS <hongjs@kr.ibm.com>
2017-02-22 11:56:42 +0900

    Integration test

---
+++ tests/integration/test-integrat1.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+var supertest = require('supertest'),
+    supertest_session = require('supertest-session');
+
+describe.skip('Integration Test 1', function() {
+
+    var testagent = supertest(app);
+
+    it.skip('test without login', function (done) {
+        testagent
+            .get('/test', {
+                input: 'input'
+            }))
+            .expect(401)
+            .end(done);
+    });
+});
